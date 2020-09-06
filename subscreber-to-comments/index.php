01d68c001f763689e5b9dc762d4b68346cdc9b0a
AnselmoNetto <70862053+AnselmoNetto@users.noreply.github.com>
2020-09-06 17:43:41 -0300

    subscreber
    exemplo para re-envio de email de resposta do post

---
+++ subscreber-to-comments/index.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @package ççlkçlk
+ * @version 0.0.1
+ */
+/*
+Plugin Name: subscreber 2 comments
+
+Description: Teste exemplos
+Author: Anselmo Paixao
+Version: 0.0.1
+*/
+
+class KDM_Subscriber{
+	static $field_name = 'kdms_subscriber';
+	
+	function init() {
+		add_action('comment_form', array('KDM_Subscriber','show_form'));
+		add_action('comment_post', array('KDM_Subscriber','check_subscriber'),10,2);
+		add_filter('wp_mail_content_type', create_function('','return "text/html";'));
+		add_action('wp_loaded', array('KDM_Subscriber','subscriber'));
+	}
+	function show_form(){
+		?>
+			<label>
+				<input type="checkbox" name="<?php echo self::$field_name;?>" value="1" />
+				Desejo receber notificações de novos comentarios.
+			</label>
+		<?php 	
+	}
+	function check_subscriber($comment_id, $approved) {
+		
+		if(isset($_POST[self::$field_name]) && $_POST[self::$field_name]){
+			$comment = array(
+					'comment_ID' => $comment_id,
+					'comment_karma'=> '1'
+					);
+			wp_update_comment($comment);
+		}
+		if($approved){
+			$comment = get_comment($comment_id);
+			self::notify($comment);
+		}
+	}
+	function notify($comment) {
+		
+		$post_id = $comment->comment_post_ID;
+		$permalink = get_permalink($post_id);
+		$messagem = sprintf(
+					'novo comentario para %.<a href="%s">clique aqui</a> para visualizar.<br>'.
+					'<a href="[unsubscribe]">cancelar notificação</a>',
+					get_the_title($post_id),
+					$permalink
+				);
+		$comments = get_comments(array(
+					'post_id'=> $post_id,
+					'karma' => '1'
+					));
+					
+		foreach ($comments as $value) {
+			
+			if($value->comment_ID != $comment->comment_id){
+				$url = add_query_arg('unsubscribe',$value->comment_ID);
+				$messagem = str_replace('[unsubscribe]',$url,$messagem);
+				wp_mail($comment->comment_author_email, 'notofocação de comentarios', $messagem);
+			}
+			
+		}
+	}
+	function subscriber() {
+		$comment_id = isset($_GET['unsubscribe'])?(int)$_GET['unsubscribe']:false;
+		if(is_single() && $comment_id){
+			$c = get_comment($comment_id);
+			if($c->comment_karma == '1'){
+				$comment = array(
+							'comment_ID'=> $comment_id,
+							'comment_karma' => '0'
+				);
+				wp_update_comment($comment);
+				
+				$msg =' Cancelamento Confirmado!';
+			}else{
+				$msg =' Já foi cancelado!';
+			}
+			wp_die($msg);
+		}
+	}
+}
+add_action('plugin_loaded', array('KDM_Subscriber','init'));
